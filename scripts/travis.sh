8fdd3b1b7a5a3ea25075ba03cd1435fc0b8512df
Yuri Gribov <tetra2005@gmail.com>
2022-01-07 13:40:44 +0300

    Use manual collection of coverage.

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -6,6 +6,7 @@
 # found in the LICENSE.txt file.
 
 set -eu
+set -x
 
 if test -n "${TRAVIS:-}" -o -n "${GITHUB_ACTIONS:-}"; then
   set -x
@@ -17,7 +18,15 @@ make "$@" clean all
 make "$@" check
 
 # Upload coverage
-if test -n "${CODECOV_TOKEN:-}"; then
+if test -n "${COVERAGE:-}"; then
+  # Collect coverage for DLL
+  mv bin/*.gc[dn][ao] src
+  gcov src/*.gcno
+  # Collect coverage for tests
+  for d in tests/*; do
+    ! test -d "$d" || (cd $d && gcov *.gcno)
+  done
+  # Upload
   curl --retry 5 -s https://codecov.io/bash > codecov.bash
-  bash codecov.bash -Z
+  bash codecov.bash -Z -X gcov
 fi
